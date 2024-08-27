823d6a97ad3ce153dace2d1ffdb968a4c4f23752
yigitcanolmez <yigitcanolmez@outlook.com>
2024-08-27 21:43:10 +0300

    solved migratory birds

---
+++ Migratory Birds/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Linq;
+
+List<int> arr = new List<int>() { 1, 4,4,4,5,3 };
+
+Dictionary<int, int> dict = new Dictionary<int, int>();
+
+var tempArr = new List<int>();
+
+tempArr.AddRange(arr);
+
+
+foreach (var item in arr)
+{
+    if (!dict.ContainsKey(item))
+    {
+        tempArr.Remove(item);
+        dict.Add(item, 0);
+    }
+    else
+        continue;
+
+    for (int i = 0; i < arr.Count; i++)
+    {
+        if (arr[i] == item)
+            dict[item] = dict[item] + 1;
+    }
+}
+
+
+int maxValue = dict.Values.Max();
+
+int minKey = dict.Where(kv => kv.Value == maxValue)
+                 .Select(kv => kv.Key)
+                 .Min();
+
+return minKey;
