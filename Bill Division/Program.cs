11c3d1ef0be089df6f184e4d2399494bd61bb341
yigitcanolmez <yigitcanolmez@outlook.com>
2024-08-29 21:50:13 +0300

    added Bill Division

---
+++ Bill Division/Program.cs
@@ -0,0 +1,12 @@
+﻿int b = 7;
+int k = 1;
+var bill = new List<int> { 3, 10, 2, 9 };
+
+var billTotal = bill.Sum();
+
+if ((billTotal / 2) == bill[k])
+    Console.WriteLine("Bon Appetit");
+else if (((billTotal - bill[k]) / 2) == b)
+    Console.WriteLine("Bon Appetit");
+else
+    Console.WriteLine(bill[k] / 2);
