0c54d7638020483a9bf28d82258b89014498423b
Yılmaz <51139602+yilmazolmez@users.noreply.github.com>
2024-08-31 21:13:21 +0300

    Create SalesByMatch_1.cs

---
+++ Sales By Match/SalesByMatch_1.cs
@@ -0,0 +1,31 @@
+List<int> ar = new();
+
+int pairCount = 0;
+
+Dictionary<int, int> keyValueForSocks = new();
+
+foreach (var sock in ar.Distinct().Order())
+{
+    keyValueForSocks.Add(sock, ar.Where(x => x == sock).Count());
+}
+
+
+foreach (var sockInfo in keyValueForSocks)
+{
+    //pairCount += sockInfo.Value / 2;
+
+    //Çift Sayı
+    if (sockInfo.Value % 2 == 0)
+	{
+        pairCount = pairCount + (sockInfo.Value / 2);
+    }
+    else //Tek Sayı
+    {
+        var doubleNumber = (double)sockInfo.Value / 2;
+        var flootNumber = (int)Math.Floor(doubleNumber);
+        pairCount = pairCount + flootNumber;
+
+    }
+}
+
+return pairCount;
