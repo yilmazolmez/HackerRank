0092985ba601054d9fa8e89963affe77041669f5
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-09-02 22:41:32 +0300

    DrawingBook3 added

---
+++ Drawing Book/DrawingBook3.cs
@@ -0,0 +1,25 @@
+﻿namespace Drawing_Book3
+{
+    public class DrawingBook3
+    {
+        public static int PageCount(int n, int p)
+        {
+            int fromStart = p / 2;
+
+            int totalPages = (n / 2);
+
+            int fromEnd = totalPages - fromStart;
+
+            if (fromEnd < fromStart)
+            {
+                return fromEnd;
+            }
+            else
+            {
+                return fromStart;
+            }
+        }
+
+
+    }
+}
--- Drawing Book/Program.cs
+++ Drawing Book/Program.cs
@@ -1,4 +1,8 @@
 ﻿using Drawing_Book;
+using Drawing_Book3;
 
  DrawingBook.PageCount(5, 4);
 DrawingBook2.PageCount(6, 5);
+
+
+DrawingBook3.PageCount(2,5);
