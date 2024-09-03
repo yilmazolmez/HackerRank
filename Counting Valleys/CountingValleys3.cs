173852396a9352f5a85cab76b73228457dccf565
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-09-03 21:25:17 +0300

    CountingValleys3 solved

---
+++ Counting Valleys/CountingValleys3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Valleys
+{
+    public static class CountingValleys3
+    {
+        public static int Solve(int steps, string path)
+        {
+            int degree = 0;
+            
+            int total = 0;
+
+            foreach (var item in path)
+            {
+                if (item == 'U')
+                {
+                    degree++;
+                }
+                else
+                {
+                    degree--;
+                }
+
+
+                if (degree == 0 && item == 'U')
+                {
+                    total++;
+                }
+            }
+
+            return total;
+        }
+    }
+}
--- Counting Valleys/Program.cs
+++ Counting Valleys/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Counting_Valleys;
+
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+CountingValleys3.Solve(2, "sdadas");
