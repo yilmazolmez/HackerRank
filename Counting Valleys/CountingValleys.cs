6531e34476b54f21d42b7fcf3bf258f33f574356
yigitcanolmez <yigitcanolmez@outlook.com>
2024-09-03 20:57:33 +0300

    added valleycount solve

---
+++ Counting Valleys/CountingValleys.cs
@@ -0,0 +1,24 @@
+﻿namespace Counting_Valleys;
+
+public class CountingValleys
+{
+    public static int CountingValley(int steps, string path)
+    {
+        int valleyCount = 0;
+        int floor = 0;
+
+        foreach (var step in path)
+        {
+            floor = step == 'U' ? floor + 1 : floor - 1;
+
+            if (floor == 0 && step == 'U')
+                valleyCount++;
+        }
+
+        return valleyCount;
+
+
+
+
+    }
+}
